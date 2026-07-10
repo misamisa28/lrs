734a82fd99cef067778ada5078138a87e788661d
misassq <queenssq@163.com>
2026-07-10 21:04:55 +0800

    修改

--- _run_pipeline.py
+++
@@ -1,8 +0,0 @@
-import json
-import pandas as pd
-
-nb = json.load(open(r'f:/lrs/statistics.ipynb', encoding='utf-8'))
-exec(compile(''.join(nb['cells'][0]['source']), 'statistics.ipynb', 'exec'), {'__name__': '__main__'})
-
-# After exec we'd need to not run main - the notebook cell probably runs on import. 
-# Better: extract only needed functions
